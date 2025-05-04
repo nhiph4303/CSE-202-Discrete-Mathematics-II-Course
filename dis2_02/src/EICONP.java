854a7697a40604cd1e88f44d959359295dc6b06e
Phan-Ngoc-Hanh-Nhi <155942839+nhiph4303@users.noreply.github.com>
2025-05-05 02:26:58 +0700

    lab2

---
+++ dis2_02/src/EICONP.java
@@ -0,0 +1,200 @@
+import java.io.*;
+import java.util.*;
+
+public class EICONP {
+
+    static InputReader sc;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        sc = new InputReader(System.in);
+
+        Vertex[] graph = readGraph();
+        int count = 0;
+        for (Vertex v : graph) {
+            if (!v.visited) {
+                dfs(v);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    public static void dfs(Vertex v) {
+        v.visited = true;
+        for (Vertex u : v.adjList) {
+            if (!u.visited) {
+                dfs(u);
+            }
+        }
+    }
+
+    public static Vertex[] readGraph() {
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        Vertex[] vertices = new Vertex[n];
+        for (int i = 0; i < n; i++) {
+            vertices[i] = new Vertex(i);
+        }
+
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            vertices[u].addAdjList(vertices[v]);
+            vertices[v].addAdjList(vertices[u]);
+        }
+
+        for (Vertex v : vertices) {
+            v.adjList.sort((v1, v2) -> v1.id - v2.id);
+        }
+
+        return vertices;
+    }
+
+    public static class Vertex {
+        public int id;
+        public boolean visited;
+        public List<Vertex> adjList = new ArrayList<>();
+
+        public Vertex(int id) {
+            this.id = id;
+        }
+
+        public void addAdjList(Vertex v) {
+            adjList.add(v);
+        }
+
+    }
+
+    static class InputReader {
+
+        private byte[] inbuf = new byte[2 << 23];
+        public int lenbuf = 0, ptrbuf = 0;
+        public InputStream is;
+
+        public InputReader(InputStream stream) throws IOException {
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = System.in;
+            lenbuf = is.read(inbuf);
+        }
+
+        public InputReader(FileInputStream stream) throws IOException {
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = stream;
+            lenbuf = is.read(inbuf);
+        }
+
+        public boolean hasNext() throws IOException {
+            if (skip() >= 0) {
+                ptrbuf--;
+                return true;
+            }
+            return false;
+        }
+
+        public String nextLine() throws IOException {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b) && b != ' ') { // when nextLine, ()
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b
+                // != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        private int readByte() {
+            if (lenbuf == -1) {
+                throw new InputMismatchException();
+            }
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0) {
+                    return -1;
+                }
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public Character nextChar() {
+            return skip() >= 0 ? (char) skip() : null;
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
